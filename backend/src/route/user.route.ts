683cefe053bf70a98a042dcdf7397c60aadfef9b
sachinpalsp917 <sachinpalsp917@gmail.com>
2024-04-03 09:53:18 +0530

    validate token

--- backend/src/route/user.route.ts
+++ backend/src/route/user.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import userController from "../controller/user.controller";
+import { jwtCheck } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.post("/create-user", userController.createCurrentUser);
+router.post("/create-user", jwtCheck, userController.createCurrentUser);
 
 export default router;
--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 type Props = {
@@ -10,20 +10,20 @@ export default function Auth0ProviderWithNavigate({ children }: Props) {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENTID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("unable to initialise auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("User", user);
+  const onRedirectCallback = () => {
     Navigate("/auth-callback");
   };
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
+      authorizationParams={{ redirect_uri: redirectUri, audience }}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
